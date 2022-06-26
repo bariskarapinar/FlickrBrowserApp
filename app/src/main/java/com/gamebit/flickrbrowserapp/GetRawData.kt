309b08f14932da2305061470bf5f88746cceffeb
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-26 18:37:55 +0300

    Download JSON

---
+++ app/src/main/java/com/gamebit/flickrbrowserapp/GetRawData.kt
@@ -0,0 +1,57 @@
+package com.gamebit.flickrbrowserapp
+
+import android.os.AsyncTask
+import android.util.Log
+import java.io.IOException
+import java.net.MalformedURLException
+import java.net.URL
+
+enum class DownloadStatus {
+    OK, IDLE, NOT_INITIALIZED, FAILED_OR_EMPTY, PERMISSIONS_ERROR, ERROR
+}
+
+class GetRawData : AsyncTask<String, Void, String>() {
+
+    private val TAG = "GETRawData"
+    private var downloadStatus = DownloadStatus.IDLE
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+    }
+
+    override fun doInBackground(vararg params: String?): String {
+        if (params[0] == null) {
+            downloadStatus = DownloadStatus.NOT_INITIALIZED
+            return  "No URL Specified"
+        }
+
+        try {
+            downloadStatus = DownloadStatus.OK
+            return  URL (params[0]).readText()
+        } catch (e: Exception) {
+            val errorMessage = when(e) {
+                is MalformedURLException -> {
+                    downloadStatus = DownloadStatus.NOT_INITIALIZED
+                    "doInBackground: Invalid URL ${e.message}"
+                }
+                is IOException -> {
+                    downloadStatus = DownloadStatus.FAILED_OR_EMPTY
+                    "doInBackground: IOException ${e.message}"
+                }
+                is SecurityException -> {
+                    downloadStatus = DownloadStatus.PERMISSIONS_ERROR
+                    "doInBackground: Security Exception ${e.message}"
+                }
+                else ->{
+                    downloadStatus = DownloadStatus.ERROR
+                    "doInBackground: Unknown error ${e.message}"
+                }
+            }
+            Log.e(TAG, errorMessage)
+            return errorMessage
+        }
+
+    }
+
+
+}
