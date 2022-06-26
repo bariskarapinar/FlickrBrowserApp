bea75d12a4b4f8e1fbd287f530c348edfecaf2ea
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-26 22:45:49 +0300

    Photo Class

---
+++ app/src/main/java/com/gamebit/flickrbrowserapp/Photo.kt
@@ -0,0 +1,9 @@
+package com.gamebit.flickrbrowserapp
+
+class Photo(val title: String, val author: String, val authorID: String,
+            val link: String, val tags: String, val image: String) {
+
+    override fun toString(): String {
+        return "Photo(title='$title', author='$author', authorID='$authorID', link='$link', tags='$tags', image='$image')"
+    }
+}
