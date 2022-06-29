e76166b836a97e30c8b98b7bf89949900ce94224
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-29 22:10:37 +0300

    onBindViewHolder and Picasso

--- app/src/main/java/com/gamebit/flickrbrowserapp/FlickrRecyclerViewAdapter.kt
+++ app/src/main/java/com/gamebit/flickrbrowserapp/FlickrRecyclerViewAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 class FlickImageViewHolder(view :View) : RecyclerView.ViewHolder(view) {
     var thumbnail: ImageView = view.findViewById(R.id.thumbnail)
@@ -32,7 +33,14 @@ class FlickrRecyclerViewAdapter(private var photoList: List<Photo>) : RecyclerVi
     }
 
     override fun onBindViewHolder(holder: FlickImageViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val photoItem = photoList[position]
+        Log.d(TAG, ".onBindViewHolder: ${photoItem.title} ---> $position")
+        Picasso.with(holder.thumbnail.context).load(photoItem.image)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(holder.thumbnail)
+
+        holder.title.text = photoItem.title
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlickImageViewHolder {
